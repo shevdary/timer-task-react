897a4f78935056b9164e1bc61b929de2936ecfd2
Daryna <shevvdary@gmail.com>
2020-12-02 21:33:12 +0200

    refactoring: delete unused components

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,28 +1,8 @@
 import React, { Component } from "react";
 import Timer from "../Timer/Timer";
 import TaskTab from "../TasksTab/TaskTab";
-import { connect } from "react-redux";
-import store from "../../store";
-import { bindActionCreators } from "redux";
-import * as actions from "../../actions/TimerActions";
-const { dispatch } = store;
-
-const { onLoad } = bindActionCreators(actions, dispatch);
 
 class MainPage extends Component {
-  state = {
-    load: false,
-    stateTimer: 0
-  };
-  componentWillMount() {
-    const { isActiveTimer } = this.state;
-  }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.state.load === true) {
-      localStorage.setItem("load", "true");
-      setInterval(() => {}, 1000);
-    }
-  }
   render() {
     return (
       <div>
@@ -32,18 +12,4 @@ class MainPage extends Component {
     );
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    currentTime: state.currentTime,
-    isLoad: state.isLoad,
-    ...state
-  };
-};
-
-const mapDispatchToProps = () => {
-  return {
-    onUpdate: onLoad
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
+export default MainPage;
--- src/components/TasksChart/TaskChart.jsx
+++ src/components/TasksChart/TaskChart.jsx
@@ -1,91 +1,95 @@
 import React, { Component } from "react";
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend,
-    ResponsiveContainer
-} from "recharts";
+//redux
 import { connect } from "react-redux";
 import moment from "moment";
 import { isDifferenceTime, unixToTime } from "../../helpers/unixToTime";
+//components
+import ButtonGenerate from "../ButtonGenerate/ButtonGenerate";
+//charts
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from "recharts";
 
 class Chart extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        };
-        this.renderTable();
-    }
-    renderTable = () => {
-        const { data } = this.state;
-        const date = data;
-        for (let i = 0; i < 24; i++) {
-            date.push({ name: i, minutes: localStorage.getItem("count") });
-        }
-        this.setState({ date: date });
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
     };
-
-    componentDidMount() {
-        this.onCharts();
+    this.renderTable();
+  }
+  renderTable = () => {
+    const { data } = this.state;
+    const date = data;
+    for (let i = 0; i < 24; i++) {
+      date.push({ name: i, minutes: localStorage.getItem("count") });
     }
+    this.setState({ date: date });
+  };
 
-    onCharts = () => {
-        const { data } = this.state;
-        const { tasks } = this.props;
-        let copyData = data;
-        tasks.forEach(item => {
-            const startTimerHour = moment(item.startTime, "HH:mm:ss");
-            const nextHour = moment(startTimerHour.hours() + 1, "HH:mm:ss");
-            const durationsInSecond = isDifferenceTime(nextHour, startTimerHour);
-            const durationToTime = moment.duration(durationsInSecond, "seconds");
-            const timerDuration = moment.duration(item.spendTime, "second");
-            const increase = durationToTime.minutes() - timerDuration.minutes();
-            if (increase > 0) {
-                copyData[startTimerHour.hours()].minutes += moment(
-                    item.spendTime,
-                    "HH:mm:ss"
-                ).minutes();
-            }
-            if (increase < 0) {
-                copyData[startTimerHour.hours()].minutes += item.spendTime;
-                copyData[startTimerHour.hours() + 1].minutes += -increase;
-            }
-        });
-    };
-    render() {
-        return (
-            <div>
-                <ResponsiveContainer minWidth="100%" minHeight="100%" aspect={5 / 2}>
-                    <BarChart
-                        data={this.state.data}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="minutes" fill="#344dc4"/>
+  componentDidMount() {
+    this.onCharts();
+  }
 
-                    </BarChart>
-                </ResponsiveContainer>
-            </div>
-        );
-    }
+  onCharts = () => {
+    const { data } = this.state;
+    const { tasks } = this.props;
+    let copyData = data;
+    tasks.forEach(item => {
+      const startTimerHour = moment(item.startTime, "HH:mm:ss");
+      const nextHour = moment(startTimerHour.hours() + 1, "HH:mm:ss");
+      const durationsInSecond = isDifferenceTime(nextHour, startTimerHour);
+      const durationToTime = moment.duration(durationsInSecond, "seconds");
+      const timerDuration = moment.duration(item.spendTime, "second");
+      const increase = durationToTime.minutes() - timerDuration.minutes();
+      if (increase > 0) {
+        copyData[startTimerHour.hours()].minutes += moment(
+          item.spendTime,
+          "HH:mm:ss"
+        ).minutes();
+      }
+      if (increase < 0) {
+        copyData[startTimerHour.hours()].minutes += item.spendTime;
+        copyData[startTimerHour.hours() + 1].minutes += -increase;
+      }
+    });
+  };
+  render() {
+    return (
+      <div>
+        <ResponsiveContainer minWidth="100%" minHeight="100%" aspect={5 / 2}>
+          <BarChart
+            data={this.state.data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="minutes" fill="#344dc4" />
+          </BarChart>
+        </ResponsiveContainer>
+        <ButtonGenerate />
+      </div>
+    );
+  }
 }
 const mapStateToProps = state => {
-    return {
-        tasks: state.tasks
-    };
+  return {
+    tasks: state.tasks
+  };
 };
 export default connect(mapStateToProps)(Chart)
