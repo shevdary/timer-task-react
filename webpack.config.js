840fa170cf498f365b7452369865d84916e463a2
Daryna <shevvdary@gmail.com>
2020-12-05 21:59:34 +0200

    fix: error loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const miniCssPlugins = require("mini-css-extract-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssPlugins = require('mini-css-extract-plugin');
+const path = require('path');
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'main.js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
-        }
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
       },
       {
         test: /\.(png|jpg|jpeg|gif|ico)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              outputPath: "images",
-              name: "[name]:[sha1:hash:3].[ext]"
-            }
-          }
-        ]
+              outputPath: 'images',
+              name: '[name]:[sha1:hash:3].[ext]',
+            },
+          },
+        ],
       },
       {
         test: /\.(css)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
+    extensions: ['*', '.js', '.jsx'],
     modules: [
-      "node_modules",
-      "bower_components",
-      "shared",
-      "/shared/vendor/modules"
-    ]
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html"
+      template: 'public/index.html',
     }),
-    new miniCssPlugins()
+    new miniCssPlugins(),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
+    contentBase: path.resolve(__dirname, './dist'),
+    compress: true,
     open: true,
     overlay: true,
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
