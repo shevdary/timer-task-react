4694d5e542120b3a3cc8a8168c172e0ee3a7bceb
Daryna <shevvdary@gmail.com>
2020-12-17 04:32:11 +0200

    set eslint config

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 // components
 import MainPage from './components/MainPage/MainPage';
 
-const App = () => {
-  return (
-    <BrowserRouter basename="/">
-      <Switch>
-        <Redirect exact from="/" to="/tasks" />
-        <Route path="/:page" component={MainPage} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter basename="/">
+    <Switch>
+      <Redirect exact from="/" to="/tasks" />
+      <Route path="/:page" component={MainPage} />
+    </Switch>
+  </BrowserRouter>
+);
 export default App;
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,6 +1,6 @@
+import { combineReducers } from 'redux';
 import tasks from './reducers/tasks';
 import timer from './reducers/timer';
-import { combineReducers } from 'redux';
 
 export default combineReducers({
   timer,
